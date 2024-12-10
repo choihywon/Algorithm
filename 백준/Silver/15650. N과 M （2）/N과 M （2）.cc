e6dac0b585e69df37e38cf8631b8584c5f9d5c08
choihyewon <144045451+choihywon@users.noreply.github.com>
2024-12-11 00:21:55 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void dfs(int n, int m, vector<int>& sequence, int start) {
+	if (sequence.size() == m) {
+		for (int num : sequence) {
+			cout << num << " ";
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = start; i <= n; i++) {
+		sequence.push_back(i);
+		dfs(n, m, sequence, i + 1);
+		sequence.pop_back();
+	}
+}
+
+int main() {
+	int n, m; 
+	cin >> n >> m;
+	vector<int> sequence;
+	dfs(n, m, sequence, 1);
+	return 0;
+}
