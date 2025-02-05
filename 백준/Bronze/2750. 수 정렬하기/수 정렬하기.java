1473a2437c4f0a0ff549129996712eb4b0c4ea38
choihyewon <144045451+choihywon@users.noreply.github.com>
2025-02-05 15:24:27 +0900

    [Bronze II] Title: 수 정렬하기, Time: 160 ms, Memory: 16352 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,37 +1,70 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Solution {
-    public void sort(int[] numbers, int k) {
-        for(int i = 0; i < k - 1; i++) {
-            int min = i;
-            for(int j = i + 1; j < k; j++) {
-                if(numbers[j] < numbers[min]) {
-                    min = j;
-                }
+    public void mergeSort(int[] num, int left, int right) {
+        if(left == right) return;
+        int mid = (left+right)/2;
+        mergeSort(num, left, mid);
+        mergeSort(num, mid+1, right);
+        merge(num, left, mid, right);
+
+    }
+    public void merge(int[] num, int left, int mid, int right) {
+        //오른쪽 길이
+        int n1 = mid - left + 1;
+        //왼쪽 길이
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        //오른쪽 배열 저장
+        for(int i = 0; i < n1; i++)
+            L[i] = num[left + i];
+        //왼쪽 배열 저장
+        for(int j = 0; j < n2; j++)
+            R[j] = num[mid + 1 + j];
+
+        //오른쪽 왼쪽 배열 인덱스
+        int i = 0, j = 0;
+        //원본배열 시작 인덱스
+        int k = left;
+        while(i < n1 && j < n2) {
+            if(L[i] <= R[j]) {
+                num[k] = L[i];
+                i++;
+            } else {
+              num[k] = R[j];
+              j++;
             }
-            int temp = numbers[i];
-            numbers[i] = numbers[min];
-            numbers[min] = temp;
+            k++;
         }
-
-
-
+        while(i < n1) {
+            num[k] = L[i];
+            i++;
+            k++;
+        }
+        while(j < n2) {
+            num[k] = R[j];
+            j++;
+            k++;
+        }
     }
 }
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] num = new int[n];
-        for(int i = 0; i < n; i++){
-            num[i] = Integer.parseInt(br.readLine());
+        int[] numbers = new int[n];
+        for(int i = 0; i < n; i++) {
+           numbers[i] = Integer.parseInt(br.readLine());
         }
         Solution solution = new Solution();
-        solution.sort(num, n);
-        for(int i = 0; i < n; i++){
-            System.out.print(num[i] + " ");
+        solution.mergeSort(numbers, 0, n-1);
+        
+        for(int i = 0; i < n; i++) {
+            System.out.print(numbers[i] + " ");
         }
     }
 }
